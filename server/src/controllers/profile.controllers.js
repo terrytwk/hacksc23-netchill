db8354847a50ead2660d524d0e8460af647fdbcd
Terry Kim <terrykim950@gmail.com>
2023-02-04 00:56:48 -0800

    working on profile endpoints

--- server/src/controllers/profile.controllers.js
+++ server/src/controllers/profile.controllers.js
@@ -2,7 +2,7 @@ const getAllTags = (req, res) => {
   // fetch all tags including career and hobby
 
   res.status(200).json({
-    message: `Fetched all the tags including career and hobby`,
+    message: `fetched all the tags including career and hobby`,
     data: {
       career: [],
       hobby: [],
@@ -10,4 +10,38 @@ const getAllTags = (req, res) => {
   });
 };
 
-module.exports = { getAllTags };
+const createProfile = (req, res) => {
+  res.status(200).json({
+    message: "created profile",
+  });
+};
+
+const getProfile = (req, res) => {
+  res.status(200).json({
+    message: "retrieved profile",
+    data: {},
+  });
+};
+
+const updateProfile = (req, res) => {
+  res.status(200).json({
+    message: "updated profile",
+  });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+
+  res.status(200).json({
+    message: `retrieved an user with user id: ${userId}`,
+    data: {},
+  });
+};
+
+module.exports = {
+  getAllTags,
+  createProfile,
+  getProfile,
+  updateProfile,
+  getUser,
+};
--- server/src/routes/profile.routes.js
+++ server/src/routes/profile.routes.js
@@ -1,3 +1,4 @@
+const { Router } = require("express");
 const express = require("express");
 
 // import controllers
@@ -6,5 +7,11 @@ const profileControllers = require("../controllers/profile.controllers");
 const router = express.Router();
 
 router.get("/tags", profileControllers.getAllTags);
+
+router.post("/me", profileControllers.createProfile);
+router.get("/me", profileControllers.getProfile);
+router.put("/me", profileControllers.updateProfile);
+
+router.get("/:userId", profileControllers.getUser);
 
 module.exports = router;
