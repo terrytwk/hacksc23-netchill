26932a530630f667ec8b2147c0425360014c9e09
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-02-04 04:24:29 -0800

    bottom drawer

---
+++ client/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class NetChillColors {
+  static const Color primary = Color(0xFF2E62B8);
+  static const Color gray = Color(0xFFA3A3A3);
+  static const Color lightGray = Color(0xFFD9D9D9);
+}
--- client/lib/pages/discover.dart
+++ client/lib/pages/discover.dart
@@ -1,6 +1,8 @@
+import 'package:bottom_drawer/bottom_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:netchill/constants/colors.dart';
 
 class DiscoverPage extends ConsumerStatefulWidget {
   const DiscoverPage({super.key});
@@ -12,21 +14,104 @@ class DiscoverPage extends ConsumerStatefulWidget {
 class _DiscoverPageState extends ConsumerState<DiscoverPage> {
   late GoogleMapController _mapController;
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  BottomDrawerController _controller = BottomDrawerController();
+  double? _height;
+
+  @override
+  void initState() {
+    super.initState();
+    // Future.delayed(Duration(milliseconds: 100), () => _showBottomSheet());
+    /// create a bottom drawer controller to control the drawer.
+    Future.delayed(
+      Duration(milliseconds: 100),
+      () => setState(() => _height = MediaQuery.of(context).size.height),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11,
-        ),
+      // body: _buildBottomDrawer(),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11,
+            ),
+          ),
+          _buildBottomDrawer(),
+        ],
       ),
     );
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
+  }
+
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      isDismissible: false,
+      // useSafeArea: true,
+      enableDrag: true,
+      builder: (context) {
+        return Column(
+          children: [
+            const Text('hello there'),
+            const SizedBox(height: 100),
+            const Text('what what'),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildBottomDrawer() {
+    final height = MediaQuery.of(context).size.height;
+    final padding = MediaQuery.of(context).viewPadding;
+    final drawerHeight = height - 1 * (padding.top + padding.bottom);
+
+    return BottomDrawer(
+      /// your customized drawer header.
+      header: SizedBox(
+        width: double.infinity,
+        child: Row(
+          children: [
+            const Expanded(child: SizedBox()),
+            Container(
+              width: 100,
+              height: 7,
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: NetChillColors.lightGray,
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+          ],
+        ),
+      ),
+
+      /// your customized drawer body.
+      body: Container(
+        child: SizedBox(child: const Text('hello body')),
+      ),
+
+      /// your customized drawer header height.
+      headerHeight: drawerHeight / 3,
+
+      /// your customized drawer body height.
+      drawerHeight: drawerHeight,
+
+      /// drawer background color.
+      color: Colors.white,
+      boxShadow: [],
+
+      /// drawer controller.
+      controller: _controller,
+    );
   }
 }
