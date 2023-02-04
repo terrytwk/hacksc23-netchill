057b6e4376682adb3c1aab63eaf4587c20814fb3
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-02-04 02:45:25 -0800

    set up bottom navigation tabs

---
+++ client/lib/pages/discover.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DiscoverPage extends ConsumerStatefulWidget {
+  const DiscoverPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends ConsumerState<DiscoverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
